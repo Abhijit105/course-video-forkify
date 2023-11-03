14cd55dc06cb7bc8d8372636724ea8f201cfa251
Abhijit105 <abhijitkumar10.5@gmail.com>
2023-11-03 17:43:54 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f60a920d-5523-44fb-86d9-0b5152bc20f2';
+export const MODAL_CLOSE_SEC = 2.5;
